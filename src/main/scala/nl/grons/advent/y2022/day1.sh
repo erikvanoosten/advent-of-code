9dd6a3c4fe39e5d3d5d8078910f34813a2b39784
Erik van Oosten <e.vanoosten@grons.nl>
2022-12-05 10:30:13 +0100

    2022 day 1

---
+++ src/main/scala/nl/grons/advent/y2022/day1.sh
@@ -0,0 +1,17 @@
+# Part 1
+
+cat src/main/resources/y2022/input-day1.txt | \
+  gawk '/^[0-9]+$/ { SUM = SUM + $1;} /^$/ { if (SUM > MAX) {MAX=SUM;}; SUM = 0;}; END { print("Max " MAX);}'
+
+# Max 71780
+
+
+# Part 2
+
+cat src/main/resources/y2022/input-day1.txt | \
+  gawk '/^[0-9]+$/ { SUM = SUM + $1;} /^$/ { print(SUM); SUM = 0;};' | \
+  sort -nr | \
+  head -n 3 | \
+  awk '{SUM = SUM + $1}; END {print SUM};'
+
+# 212489
