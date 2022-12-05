11e80f7e63984a781ee47a1b5d6dc463e4119009
Erik van Oosten <e.vanoosten@grons.nl>
2022-12-05 10:30:24 +0100

    2022 day 2

---
+++ src/main/scala/nl/grons/advent/y2022/Day2Part1.scala
@@ -0,0 +1,53 @@
+package nl.grons.advent.y2022
+
+import zio.stream.ZPipeline.{splitLines, utfDecode}
+import zio.stream.ZStream
+import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
+
+import java.io.File
+
+object Day2Part1 extends ZIOAppDefault {
+
+  abstract class RPS(val value: Int)
+  object Rock extends RPS(1)
+  object Paper extends RPS(2)
+  object Scissors extends RPS(3)
+
+  object RPS {
+    def fromElf(elf: String): RPS = elf match {
+      case "A" => Rock
+      case "B" => Paper
+      case "C" => Scissors
+      case "X" => Rock
+      case "Y" => Paper
+      case "Z" => Scissors
+    }
+
+    def scoreForSecond(first: RPS, second: RPS): Int = {
+      val outcome = (first, second) match {
+        case (f, s) if f == s => 3
+        case (Rock, Paper) => 6
+        case (Rock, Scissors) => 0
+        case (Paper, Rock) => 0
+        case (Paper, Scissors) => 6
+        case (Scissors, Rock) => 6
+        case (Scissors, Paper) => 0
+      }
+      outcome + second.value
+    }
+  }
+
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
+    ZStream
+      .fromFile(new File("src/main/resources/y2022/input-day2.txt"))
+      .via(utfDecode >>> splitLines)
+      .map(_.split(' '))
+      .map { case Array(elf1, elf2) =>
+        RPS.scoreForSecond(RPS.fromElf(elf1), RPS.fromElf(elf2))
+      }
+      .runSum
+      .flatMap(score => zio.Console.printLine("Score: " + score)) // 13221
+  }
+
+}
---
+++ src/main/scala/nl/grons/advent/y2022/Day2Part2.scala
@@ -0,0 +1,79 @@
+package nl.grons.advent.y2022
+
+import zio.stream.ZPipeline.{splitLines, utfDecode}
+import zio.stream.ZStream
+import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
+
+import java.io.File
+
+object Day2Part2 extends ZIOAppDefault {
+
+  sealed abstract class RPS(val value: Int)
+  object Rock extends RPS(1)
+  object Paper extends RPS(2)
+  object Scissors extends RPS(3)
+
+  object RPS {
+    def fromElf(elf: String): RPS = elf match {
+      case "A" => Rock
+      case "B" => Paper
+      case "C" => Scissors
+    }
+
+    def scoreForSecond(first: RPS, second: RPS): Int = {
+      val outcome = (first, second) match {
+        case (Rock, Paper) => 6
+        case (Rock, Scissors) => 0
+        case (Paper, Rock) => 0
+        case (Paper, Scissors) => 6
+        case (Scissors, Rock) => 6
+        case (Scissors, Paper) => 0
+        case _ => 3
+      }
+      outcome + second.value
+    }
+  }
+
+  sealed abstract class Outcome(val value: Int)
+  object Win extends Outcome(6)
+  object Draw extends Outcome(3)
+  object Loose extends Outcome(0)
+
+  object Outcome {
+    def fromInput(elf: String): Outcome = elf match {
+      case "X" => Loose
+      case "Y" => Draw
+      case "Z" => Win
+    }
+
+    def selectRpsForOutcome(elf1: RPS, outcomeForSecond: Outcome): RPS = outcomeForSecond match {
+      case Win => elf1 match {
+        case Paper => Scissors
+        case Rock => Paper
+        case Scissors => Rock
+      }
+      case Draw => elf1
+      case Loose => elf1 match {
+        case Scissors => Paper
+        case Paper => Rock
+        case Rock => Scissors
+      }
+    }
+  }
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
+    ZStream
+      .fromFile(new File("src/main/resources/y2022/input-day2.txt"))
+      .via(utfDecode >>> splitLines)
+      .map(_.split(' '))
+      .map { case Array(col1, col2) =>
+        val rpsElf1 = RPS.fromElf(col1)
+        val outcomeElf2 = Outcome.fromInput(col2)
+        val rpsElf2 = Outcome.selectRpsForOutcome(rpsElf1, outcomeElf2)
+        RPS.scoreForSecond(rpsElf1, rpsElf2)
+      }
+      .runSum
+      .flatMap(score => zio.Console.printLine("Score: " + score)) // 13131
+  }
+
+}
